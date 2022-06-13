f14d04960c7144dd08798c40bc68dbdb125976ae
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-06-13 01:04:48 -0400

    commit 8

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import "./About.css";
+//import <profile_pic_name> from "../assets/profile_pic_name.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">William Monsalve</div>
+            <div className="brief_description">
+            <p>I'm passionate about programming, chess, playing my guitar, and making new connections.</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
